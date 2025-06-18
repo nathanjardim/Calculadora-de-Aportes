29d6ae3ff31600a16fd37bd152dd880cd49b5bb7
nathanjardim <nathan.henrique_@live.com>
2025-06-17 22:00:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,7 +159,7 @@ _, _, patrimonio = simular_aposentadoria(
         )
 
 anos_aporte = dados["idade_aposentadoria"] - dados["idade_atual"]
-        percentual = int(aporte / dados["renda_atual"] * 100)
+percentual = int(aporte / dados["renda_atual"] * 100)
         patrimonio_final = int(patrimonio[(anos_aporte) * 12])
         aporte_int = int(aporte)
 
